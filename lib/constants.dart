9de96c6937bd3b71519c98d8c23ae2f0fd3f01a9
Renato Rosa Franco <Renato_ny@live.com>
2024-03-04 00:19:07 -0300

    Update videoController and implemented AuthController.

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok/controllers/auth_controller.dart';
 
 // PAGES
 List pages = [];
@@ -16,3 +17,6 @@ var firestore = FirebaseFirestore.instance;
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
+
+// CONTROLLER
+var authController = AuthController.instance;
